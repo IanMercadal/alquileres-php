fabd60156a0ce088a90e36af41af0b44fe55a50a
Ian Mercadal <imercadal@cifpfbmoll.eu>
2023-02-05 13:37:50 +0100

    feat: :art: Mejorando css
    Fondo y estilos ya creados

--- views/index.php
+++ views/index.php
@@ -49,7 +49,7 @@
 <section class="seccion">
     <h3>Casas</h3>
 
-    <div class="tres-columnas">
+    <div class="cuatro-columnas">
         <div class="columna">
             <img class="casa-imagen-stock" src="<?php echo base_url ?>/assets/img/utils/example-2.jpg">
             <div class="casa-info-stock">
@@ -63,6 +63,17 @@
         </div>
         <div class="columna">
             <img class="casa-imagen-stock" src="<?php echo base_url ?>/assets/img/utils/example-3.jpg">
+            <div class="casa-info-stock">
+                <h4>Casa Rústica</h4>
+                <p><b>Localización:</b> Valldemossa</p>
+                <div class="dos-columnas">
+                    <p><b>2.000 $</b></p>
+                    <button class="btn-primary">Ver</button>
+                </div>
+            </div>
+        </div>
+        <div class="columna">
+            <img class="casa-imagen-stock" src="<?php echo base_url ?>/assets/img/utils/example-4.jpg">
             <div class="casa-info-stock">
                 <h4>Casa Rústica</h4>
                 <p><b>Localización:</b> Valldemossa</p>
--- views/layout/footer.php
+++ views/layout/footer.php
@@ -18,16 +18,17 @@
                 <path d="M22 4.01c-1 .49 -1.98 .689 -3 .99c-1.121 -1.265 -2.783 -1.335 -4.38 -.737s-2.643 2.06 -2.62 3.737v1c-3.245 .083 -6.135 -1.395 -8 -4c0 0 -4.182 7.433 4 11c-1.872 1.247 -3.739 2.088 -6 2c3.308 1.803 6.913 2.423 10.034 1.517c3.58 -1.04 6.522 -3.723 7.651 -7.742a13.84 13.84 0 0 0 .497 -3.753c-.002 -.249 1.51 -2.772 1.818 -4.013z" />
             </svg>
         </div>
+        <span>Calle Invent 25, 1 derecha</span>
+        <span><b>Teléfono:</b> 987654321</span>
     </div>
     <nav class="navigation-footer">
-        <h3>Links</h3>
+        <h3>Navigation</h3>
         <div class="nav-options-footer">
             <a href="" class="nav-link-footer">Inicio</a>
             <a href="" class="nav-link-footer">Casas</a>
             <a href="" class="nav-link-footer">Nosotros</a>
             <a href="" class="nav-link-footer">Aviso Legal</a>
         </div>
-        <p>Todos los derechos reservados &copy; 2023</p>
     </nav>
 </footer>
     
